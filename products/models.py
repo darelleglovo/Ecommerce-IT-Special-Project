780e38d93f9647ab6014cfd70110e69b74398ca5
Darelle <darelleglovo02@gmail.com>
2018-07-21 00:08:24 +0800

    slug field | product URLs

--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
 import random
 import os
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -41,12 +44,25 @@ class ProductManager(models.Manager):
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, unique=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured = models.BooleanField(default=False)
     active =  models.BooleanField(default=True)
 
-    objects = ProductManager()
+    objects = ProductManager() # Product.objects.(something)
+
+    def get_absolute_url(self):
+        return "/products/{slug}/".format(slug=self.slug)
+
     def __str__(self):
         return self.title
+    def __unicode__(self):
+        return self.title
+
+def product_pre_save_reciever(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(product_pre_save_reciever, sender=Product)
--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductListView.as_view()),
-    path('featured/', views.ProductFeaturedListView.as_view()),
-    path('featured/<int:pk>/', views.ProductFeaturedDetailView.as_view()),
-    path('products-fbv/', views.product_list_view),
-    path('products/<int:pk>/', views.ProductDetailView.as_view()),
-    path('products-fbv/<int:pk>/', views.product_detail_view),
+    path('products/<slug:slug>/', views.ProductDetailSlugView.as_view()),
+    # path('featured/', views.ProductFeaturedListView.as_view()),
+    # path('featured/<int:pk>/', views.ProductFeaturedDetailView.as_view()),
+    # path('products-fbv/', views.product_list_view),
+    # path('products/<int:pk>/', views.ProductDetailView.as_view()),
+    # path('products-fbv/<int:pk>/', views.product_detail_view),
 ]
